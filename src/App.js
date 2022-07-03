78ab32c06ae378c8b3e845f829781392520ef3bc
Keshav Sharma <46739493+sharmakeshav10@users.noreply.github.com>
2022-07-04 01:08:51 +0530

    user emoji input handler

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import "./styles.css";
 import React, { useState } from "react";
 
+const emojiDict = {
+  "😀": "Grinning Face",
+  "😂": "Face with Tears of Joy",
+  "😉": "Winking Face",
+  "😡": "Enraged Face",
+  "❤️": "love",
+  "🤡": "Clown Face",
+  "🤓": "Nerd Face",
+  "🤩": "Star-Struck",
+  "👏": "Clapping Hands",
+  "🙏": "Folded Hands",
+  "😪": "Sleepy"
+};
+
+const emojis = Object.keys(emojiDict);
+
 export default function App() {
-  // const [userInput, setUserInput] = useState("");
+  const [emoji, setEmoji] = useState("");
+  const [meaning, setMeaning] = useState("");
 
-  // function inputHandler(event) {
-  //   setUserInput(event.target.value);
-  // }
+  function changeHandler(event) {
+    const inputEmoji = event.target.value;
+    setEmoji(inputEmoji);
 
-  return <div className="App"></div>;
+    if (inputEmoji in emojiDict) {
+      setMeaning(emojiDict[inputEmoji]);
+    } else {
+      setMeaning("Can't recognize the emoji!");
+    }
+  }
+
+  return (
+    <div className="App">
+      <h1 style={{ marginTop: "3rem" }}>Let's crack emojis</h1>
+      <input onChange={changeHandler} placeholder={"Type your emoji here..."} />
+      <h2>{emoji}</h2>
+      <h3>{meaning}</h3>
+    </div>
+  );
 }
