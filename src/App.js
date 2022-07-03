ba8801cc4b91b0c243bab2ccc1afed9330255917
Keshav Sharma <46739493+sharmakeshav10@users.noreply.github.com>
2022-07-03 22:39:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import React, { useState } from "react";
+
+export default function App() {
+  // const [userInput, setUserInput] = useState("");
+
+  // function inputHandler(event) {
+  //   setUserInput(event.target.value);
+  // }
+
+  return <div className="App"></div>;
+}
